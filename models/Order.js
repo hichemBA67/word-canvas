136cb7930d95f2ba1c9d03ad1270325b63dafe4b
uqpyx <80069620+hichemBA67@users.noreply.github.com>
2023-11-03 20:19:26 +0100

    Remove Orders

--- models/Order.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const OrderSchema = new mongoose.Schema({
-  filename: String,
-  createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = Order = mongoose.model("order", OrderSchema);
